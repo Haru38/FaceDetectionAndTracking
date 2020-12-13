2abc7113d4e405859ab7f12408c27112fb6d3c88
Haru38 <67744833+Haru38@users.noreply.github.com>
2020-12-13 16:13:19 +0900

    Update getVec.js

--- getVec.js
+++ getVec.js
@@ -1,13 +1,13 @@
 var admin = require('firebase-admin');
-var serviceAccount = require('./facetracking-67293-firebase-adminsdk-yvqw7-bee8b09b2d.json');
+var serviceAccount = require('#######.json');
 
 admin.initializeApp( {
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://facetracking-67293.firebaseio.com/" //データベースのURL
+    databaseURL: "#######" //データベースのURL
 } );
 
 var db = admin.database();
-db.ref('saikochan-info').on('child_added', function (obj) {
+db.ref('info').on('child_added', function (obj) {
     console.log(obj.val());
 });
 db.ref('coordinate').on('child_added', function (obj) {
